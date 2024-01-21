be24dfbb0d8a516d62f4b02e4d9598cca2af231a
yusufoner77 <yusufoner7@gmail.com>
2024-01-21 16:36:01 -0600

    added comments

--- where.cpp
+++ where.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 using namespace std;
 
+
+// This function will find a char in a string and return the first index of where it was found
+// if the char is not found in the string, it will output -1
+
 void find_it(string input, char search_query) {
 	
 	vector<char> letters;
@@ -27,6 +31,9 @@ void find_it(string input, char search_query) {
 
 }
 
+// This function will find a substring in a string, and return the index of where it was found
+// if the substring is not found, it will output -1
+
 void find_it(string input, string search_query) {
 	
 	if (input.find(search_query) != string::npos) {
@@ -45,12 +52,25 @@ void find_it(string input, string search_query) {
 int main() {
 	string input = "hello my name is Yusuf";
 	
+	// expected value 1
 	find_it(input, 'e');
+	
+	//expected value 5
 	find_it(input, ' ');
+	
+	//expected value -1
 	find_it(input, 'z');
+	
+	//expected value 6
 	find_it(input, "my");
+	
+	//expected value -1
 	find_it(input, "was");
+	
+	//expected value 9
 	find_it(input, "na");
+	
+	//expected value 17
 	find_it(input, "Yusuf");
 
 }
